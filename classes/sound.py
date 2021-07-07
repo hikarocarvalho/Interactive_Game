6b190dc14cef9c42f86eaf8913b5b4685ea04d77
HIKARO-290 <hikarofcarvalho@gmail.com>
2021-07-07 10:29:53 -0300

    removed sound.py and background.mp3

--- classes/sound.py
+++
@@ -1,6 +0,0 @@
-import pygame
-from pygame import mixer
-class Sound:
-    pygame.mixer.init()
-    initialize the mixer module
-    init(frequency=44100, size=-16, channels=2, buffer=512, devicename=None, allowedchanges=AUDIO_ALLOW_FREQUENCY_CHANGE | AUDIO_ALLOW_CHANNELS_CHANGE) -> None
